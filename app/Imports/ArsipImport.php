1abb585e3f8f93add4b9d6f8af6cd7f8259e46e0
vigyanmelati <gyanixc11@gmail.com>
2026-02-01 18:24:11 +0800

    UPDATE USUL MUSNAH

--- app/Imports/ArsipImport.php
+++ app/Imports/ArsipImport.php
@@ -150,9 +150,9 @@ class ArsipImport implements ToModel, WithHeadingRow
             } elseif ($sekarang <= $inaktifSampai) {
                 $result['status_arsip'] = 'INAKTIF';
             } elseif ($sekarang <= $musnahSampai) {
-                $result['status_arsip'] = 'MUSNAH';
+                $result['status_arsip'] = 'USUL_MUSNAH';
             } else {
-                $result['status_arsip'] = 'MUSNAH';
+                $result['status_arsip'] = 'USUL_MUSNAH';
             }
         } else {
             if ($sekarang <= $aktifSampai) {
